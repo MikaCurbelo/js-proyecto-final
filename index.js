48d36b526c0015104a68fbb2236d2ba5c4d4831a
Mikael Curbelo <101370513+MikaCurbelo@users.noreply.github.com>
2022-05-02 18:07:21 -0300

    desafio complementario Agregar arrays

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function saludar(nombre){
+/* function saludar(nombre){
     let saludo = "Hola " + nombre;
     alert(saludo)
 }
@@ -9,15 +9,15 @@ let respuesta = prompt("¿Conoces algun tema de nuestro ep?")
 
 if(respuesta == "si"){
     let tema = prompt("¿Cuál es?");
-    if(tema === "Disfraz" || "disfraz"){
+    if(tema === "Disfraz" || tema === "disfraz"){
         alert("Ingresaste " + tema +", entra a la pagina y conoce más sobre la banda.");
-    }else if(respuesta === "Mi verdad" || "mi verdad"){
+    }else if(tema === "Mi verdad" ||tema === "mi verdad"){
         alert("Ingresaste " + tema +", entra a la pagina y conoce más sobre la banda.");
-    }else if(respuesta === "Mochilero"|| "mochilero"){
+    }else if(tema === "Mochilero"||tema === "mochilero"){
         alert("Ingresaste " + tema +", entra a la pagina y conoce más sobre la banda.");
-    }else if(respuesta === "Arde"||"arde"){
+    }else if(tema === "Arde"||tema === "arde"){
         alert("Ingresaste " + tema +", entra a la pagina y conoce más sobre la banda.");
-    }else if(respuesta=== "Realidad"||"realidad"){
+    }else if(tema === "Realidad"||tema === "realidad"){
         alert("Ingresaste " + tema +", entra a la pagina y conoce más sobre la banda.");
     }else{
         alert("Ese tema no es nuestro");
@@ -25,6 +25,34 @@ if(respuesta == "si"){
 }
 else{
     alert("No pasa nada, ingresa a la pagina y conoce sobre nosotros")
+} */
+
+/* DESAFIO COMPLEMENTARIO AGREGAR ARRAYS */
+
+let respuesta = prompt("Conoces alun trema de Por un Día?")
+
+if(respuesta === "si" || respuesta === "Si"){
+   
+const temasDeLaBanda = [];
+let   cantidad     = 5;
+
+do{
+   let entrada = prompt("Ingresar nombre");
+   temasDeLaBanda.push(entrada);
+   console.log(temasDeLaBanda.length);
+}while(temasDeLaBanda.length != cantidad)
+const lista = temasDeLaBanda.concat(["Esta lista no queda acá, este año grabaremos muchos más!!!"]);
+alert(lista.join("\n"));
+}else{
+    alert("No pasa nada, ingresa y conocenos")
 }
+
+
+
+
+
+
+
+
 
 
