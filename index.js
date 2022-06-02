6b0aaf175885f002919b37c8cc1267ba68ce1fd9
Mikael Curbelo <101370513+MikaCurbelo@users.noreply.github.com>
2022-06-02 11:15:12 -0300

    desafio fetch, creación de data.json, agregada de pausado al resproductor musical

--- index.js
+++ index.js
@@ -1,73 +1,108 @@
 
-const infoTema = [
-    {id:1, nombre: "Mi verdad", duracion: 3.35},
-    {id:2, nombre: "Disfraz", duracion: 5.04},
-    {id:3, nombre: "Mochilero", duracion: 4.48},
-    {id:4, nombre: "Realidad", duracion: 4.45},
-    {id:5, nombre: "Arde", duracion: 4.32},
-]
-
 
 let temas = document.getElementById("temas");
 temas.innerHTML = "<h4 class=titulo2 >Lista de canciones</h4> <p class=borderNone>1- Mochilero <br> 2- Disfraz <br> 3- Mi verdad <br> 4- Realidad <br> 5- Arde(Vivo)</p>"
 
- 
-   let tema1 = document.getElementById('mochilero'); 
-   tema1.addEventListener('click', presionar1); 
+//  TEMA 1
+   let tema1Pl = document.getElementById('mochileroPl'); 
+   tema1Pl.addEventListener('click', presionar1); 
+
+   let tema1Pa = document.getElementById('mochileroPa'); 
+   tema1Pa.addEventListener('click', pausar1)
+
 
 function presionar1() { 
    let moch = document.getElementById('moch'); 
-   moch.play(); 
+      moch.play();   
 } 
+function pausar1(){
+      moch.pause();
+}
 
+// TEMA 2
+   let tema2Pl = document.getElementById('disfrazPl'); 
+   tema2Pl.addEventListener('click', presionar2);
 
-   let tema2 = document.getElementById('disfraz'); 
-   tema2.addEventListener('click', presionar2); 
+   let tema2Pa = document.getElementById('disfrazPa');
+   tema2Pa.addEventListener('click', pausar2)
+
 
 function presionar2() { 
    let dis = document.getElementById('dis'); 
   dis.play(); 
 } 
-   let tema3 = document.getElementById('miVerdad'); 
-   tema3.addEventListener('click', presionar3); 
+
+function pausar2(){
+   dis.pause();
+}
+
+
+// TEMA 3
+   let tema3Pl = document.getElementById('miVerdadPl'); 
+   tema3Pl.addEventListener('click', presionar3); 
+
+   let tema3Pa = document.getElementById('miVerdadPa');
+   tema3Pa.addEventListener('click', pausar3)
+
 
 function presionar3() { 
    let miVer = document.getElementById('miV'); 
    miVer.play(); 
 } 
-   let tema4 = document.getElementById('realidad'); 
-   tema4.addEventListener('click', presionar4); 
+function pausar3(){
+   miV.pause();
+}
+
+
+// TEMA 4
+   let tema4Pl = document.getElementById('realidadPl'); 
+   tema4Pl.addEventListener('click', presionar4); 
+
+   let tema4Pa = document.getElementById('realidadPa');
+   tema4Pa.addEventListener('click', pausar4);
+
 
 function presionar4() { 
    let real = document.getElementById('real'); 
    real.play(); 
 } 
-   let tema5 = document.getElementById('arde'); 
-   tema5.addEventListener('click', presionar5); 
+
+function pausar4() {
+real.pause();
+}
+
+// TEMA 5
+   let tema5Pl = document.getElementById('ardePl'); 
+   tema5Pl.addEventListener('click', presionar5); 
+
+   let team5Pa = document.getElementById('ardePa');
+   team5Pa.addEventListener('click', pausar5);
+
 
 function presionar5() { 
    let ard = document.getElementById('ard'); 
    ard.play(); 
 } 
 
+function pausar5(){
+   ard.pause();
+}
 
 
 
+fetch('./data.json')
+.then((resp) => resp.json())
+.then(data => {
+   const enLStorage = JSON.stringify(data);
+   console.log(enLStorage);
+   localStorage.setItem("ListaDeTemas", enLStorage);
+   
+   const recuperarLista = localStorage.getItem("ListaDeTemas");
+   console.log(JSON.parse(recuperarLista));
+
+})
 
 
 
-
-
-
-
-
-
-
-const enLStorage = JSON.stringify(infoTema);
-console.log(enLStorage);
-localStorage.setItem("ListaDeTemas", enLStorage);
-
-const recuperarLista = localStorage.getItem("ListaDeTemas");
-console.log(JSON.parse(recuperarLista));
 
 
